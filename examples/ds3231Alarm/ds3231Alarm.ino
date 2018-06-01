48c022356f61a51766d66cae0bf6ccbcdfaf8504
flaviohernan <flaviohernanfnunes@gmail.com>
2018-06-01 16:12:48 -0300

    sketch to test the output interrupt of DS3231, alarm type DS3231_ALM1_EVERY_SECOND

--- examples/ds3231Alarm/ds3231Alarm.ino
+++ examples/ds3231Alarm/ds3231Alarm.ino
@@ -4,6 +4,7 @@
 
 RTC_DS3231 rtc;
 
+
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup () {
@@ -16,7 +17,7 @@ void setup () {
 
   delay(3000); // wait for console opening
 
-  if (! rtc.begin()) {
+  if ( rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
@@ -29,9 +30,51 @@ void setup () {
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
+
+  pinMode (2, INPUT_PULLUP); // interrupt of RTC
+  pinMode (13, OUTPUT); //Arduino Led indicator
+
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  DateTime nowAlarm1;
+  nowAlarm1 = DateTime (F(__DATE__), F(__TIME__));
+  rtc.disableAlarm1();
+  rtc.clearFlagAlarm1();
+  /*
+  DS3231_ALARM_TYPES_t :
+    DS3231_ALM1_EVERY_SECOND      //every_second
+    DS3231_ALM1_MATCH_SECONDS     //match seconds
+    DS3231_ALM1_MATCH_MINUTES     //match minutes *and* seconds
+    DS3231_ALM1_MATCH_HOURS       //match hours *and* minutes, seconds
+    DS3231_ALM1_MATCH_DATE        //match date *and* hours, minutes, seconds
+    DS3231_ALM1_MATCH_DAY         //match day *and* hours, minutes, seconds
+  */
+  rtc.adjustAlarm1(nowAlarm1, DS3231_ALM1_EVERY_SECOND);
+  rtc.enableAlarm1();
+
+  Serial.print("Alarme type: ");
+  Serial.print("DS3231_ALM1_EVERY_SECOND");
+  Serial.print(nowAlarm1.year(), DEC);
+  Serial.print('/');
+  Serial.print(nowAlarm1.month(), DEC);
+  Serial.print('/');
+  Serial.print(nowAlarm1.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[nowAlarm1.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(nowAlarm1.hour(), DEC);
+  Serial.print(':');
+  Serial.print(nowAlarm1.minute(), DEC);
+  Serial.print(':');
+  Serial.print(nowAlarm1.second(), DEC);
+  Serial.println();
 }
 
 void loop () {
+  if (!digitalRead(2)) {
+    rtc.clearFlagAlarm1();
+    digitalWrite(13,(!digitalRead(13)));
+
     DateTime now = rtc.now();
     
     Serial.print(now.year(), DEC);
@@ -73,5 +116,6 @@ void loop () {
     Serial.println();
     
     Serial.println();
-    delay(3000);
+    
+    }
 }
